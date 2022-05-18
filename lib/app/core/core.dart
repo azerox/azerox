61cdbf7ddfa79683e5d0078296e0e26e5d6d698e
David Araujo <david@flutterando.com.br>
2022-05-18 08:45:24 -0300

    Carregamento de Capítulo e Envio de áudio (#26)
    Carregamento de capítulo
     - Compactação de imagem movida para quando enviar;
     - Mensagem de carregamento enquanto está enviando o capítulo;
    
    Corrigido envio de Áudio
     - A URL retornada pelo backend estava sendo alterada incorretamente antes de criar o capítulo. Ao resolver esse problema, o áudio enviado funcionou.

--- lib/app/core/core.dart
+++ lib/app/core/core.dart
@@ -1,5 +1,5 @@
 export 'controllers/audio_controller.dart';
-export 'mixins/notifier_loading_mixin.dart';
+export 'controllers/loading_controller.dart';
 export 'ui/widgets/app_drop.dart';
 export 'ui/widgets/app_field.dart';
 export 'ui/widgets/audio_player_widget.dart';
