163915637a8e1576820ee7bf436b4ae4a45f40c4
Wladimir Bandeira <tecnico100@gmail.com>
2022-06-22 16:17:46 -0300

    corrigindo navegabilidade das páginas em relação ao menu

--- lib/app/modules/net/net_page.dart
+++ lib/app/modules/net/net_page.dart
@@ -1,4 +1,6 @@
-
+import 'package:azerox/app/config/app_colors.dart';
+import 'package:azerox/app/config/app_images.dart';
+import 'package:azerox/app/modules/home/widgets/drawer/custom_drawer.dart';
 import 'package:azerox/app/modules/net/net_controller.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -11,7 +13,34 @@ class NetPage extends GetView<NetController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Net'),
+        toolbarHeight: 65,
+        title: Image.asset(AppImages.logo),
+        centerTitle: true,
+      ),
+      drawer: const CustomDrawer(),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Container(
+              height: 30,
+              color: AppColors.primary,
+              child: Row(
+                children: [
+                  const SizedBox(width: 10),
+                  Image.asset(
+                    AppImages.net1,
+                  ),
+                  const SizedBox(width: 10),
+                  const Text(
+                    'Net',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
