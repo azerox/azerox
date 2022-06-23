fd244e809a61194fa4553a8c33f9f1a7b44bb9e4
Wladimir Bandeira <tecnico100@gmail.com>
2022-06-23 16:50:57 -0300

    melhorando a largura da quarta imagem do carrossel

--- lib/app/modules/initial/initial_page.dart
+++ lib/app/modules/initial/initial_page.dart
@@ -55,7 +55,7 @@ class _InitialPageState extends State<InitialPage>
                           Center(
                             child: Image.asset(
                               AppImages.azerox4,
-                              width: MediaQuery.of(context).size.width * 0.85,
+                              width: MediaQuery.of(context).size.width * 0.78,
                               fit: BoxFit.fill,
                             ),
                           )
