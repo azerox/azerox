c5fd731f9cdb1d378dc9debaff38a36b56f4ae04
Wladimir Bandeira <tecnico100@gmail.com>
2022-07-12 16:47:38 -0300

    Foi implementado uma mensagem de aviso caso não tenha redes sociais cadastradas e foi incluídos os ícones de redes sociais

--- lib/app/modules/configuration/widget/SocialNetworksWidget.dart
+++ lib/app/modules/configuration/widget/SocialNetworksWidget.dart
@@ -1,7 +1,9 @@
 import 'package:azerox/app/config/app_images.dart';
 import 'package:azerox/app/models/social_networks.dart';
+import 'package:azerox/app/modules/configuration/visited_house_controller.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SocialNetworksWidget extends StatefulWidget {
   final SocialNetworks redeSocial;
@@ -17,8 +19,8 @@ class SocialNetworksWidget extends StatefulWidget {
 class _SocialNetworksWidgetState extends State<SocialNetworksWidget> {
   @override
   Widget build(BuildContext context) {
+    final controller = Get.find<VisitedHouseController>();
     return Container(
-      height: MediaQuery.of(context).size.height * 0.5,
       child: Padding(
         padding: const EdgeInsets.all(10),
         child: Column(
@@ -26,9 +28,65 @@ class _SocialNetworksWidgetState extends State<SocialNetworksWidget> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Visibility(
-                    visible: true,
-                    child: Text("sadjbkdsbkjbkjsandkjn")),
+                Row(
+                  children: [
+                    Visibility(
+                      visible: widget.redeSocial.network.toString() == 'Site',
+                      child: Image.asset(
+                        AppImages.net2,
+                      ),
+                    ),
+                    Visibility(
+                      visible:
+                          widget.redeSocial.network.toString() == 'Youtube',
+                      child: Image.asset(
+                        AppImages.youtube,
+                      ),
+                    ),
+                    Visibility(
+                      visible: widget.redeSocial.network.toString() == 'Map',
+                      child: Image.asset(
+                        AppImages.snapchat,
+                      ),
+                    ),
+                    Visibility(
+                      visible: widget.redeSocial.network.toString() == 'Facebook',
+                      child: Image.asset(
+                        AppImages.facebook,
+                      ),
+                    ),
+                    Visibility(
+                      visible: widget.redeSocial.network.toString() == 'Twitter',
+                      child: Image.asset(
+                        AppImages.twitter,
+                      ),
+                    ),
+                    Visibility(
+                      visible: widget.redeSocial.network.toString() == 'Flickr',
+                      child: Image.asset(
+                        AppImages.flickr,
+                      ),
+                    ),
+                    Visibility(
+                      visible: widget.redeSocial.network.toString() == 'Linkedin',
+                      child: Image.asset(
+                        AppImages.linkedin,
+                      ),
+                    ),
+                    Visibility(
+                      visible: widget.redeSocial.network.toString() == 'Instagram',
+                      child: Image.asset(
+                        AppImages.instagram,
+                      ),
+                    ),
+                    Visibility(
+                      visible: widget.redeSocial.network.toString() == 'Pintrest',
+                      child: Image.asset(
+                        AppImages.pinterest,
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
           ],
