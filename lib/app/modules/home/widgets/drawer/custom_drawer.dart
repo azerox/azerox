f4dd299e513a9e4e5c587d0abb1edbf4266a676c
Wladimir Bandeira <tecnico100@gmail.com>
2022-07-15 13:01:35 -0300

    aumentando o tamanho do menu

--- lib/app/modules/home/widgets/drawer/custom_drawer.dart
+++ lib/app/modules/home/widgets/drawer/custom_drawer.dart
@@ -27,74 +27,75 @@ class _CustomDrawerState extends State<CustomDrawer> {
   @override
   Widget build(BuildContext context) {
     final controller = Get.find<HomeController>();
-    final _services = <UserModel>[].obs;
-
-    return Drawer(
-      backgroundColor: const Color(0XFF007E94),
-      child: SafeArea(
-        child: ListView(
-          children: [
-            Container(
-              color: const Color(0XFF005E6C),
-              padding: const EdgeInsets.all(10),
-              child: TextFormField(
-                controller: controller.searchDrawerEC,
-                onChanged: (value) {
-                  setState(() {});
-                },
-                style: const TextStyle(color: Colors.black),
-                decoration: InputDecoration(
-                  hintText: 'Procurar novos editores',
-                  prefixIcon: Icon(Icons.search),
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      controller.searchDrawerEC.clear();
-                      controller.searchDrawerEC.text = "";
-                      setState(() {});
-                    },
-                    icon:
-                        const Icon(Icons.close, color: Colors.black, size: 10),
+    return Container(
+      width: 315,
+      child: Drawer(
+        backgroundColor: const Color(0XFF007E94),
+        child: SafeArea(
+          child: ListView(
+            children: [
+              Container(
+                color: const Color(0XFF005E6C),
+                padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                child: TextFormField(
+                  controller: controller.searchDrawerEC,
+                  onChanged: (value) {
+                    setState(() {});
+                  },
+                  style: const TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
+                    hintText: 'Procurar novos editores',
+                    prefixIcon: Icon(Icons.search),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        controller.searchDrawerEC.clear();
+                        controller.searchDrawerEC.text = "";
+                        setState(() {});
+                      },
+                      icon:
+                          const Icon(Icons.close, color: Colors.black, size: 10),
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
                   ),
-                  filled: true,
-                  fillColor: Colors.white,
                 ),
               ),
-            ),
-            Visibility(
-              visible: controller.searchDrawerEC.text.length < 3,
-              child: DrawerContentWidget(
-                editores: widget.editores,
-                showEditores: widget.showEditores,
+              Visibility(
+                visible: controller.searchDrawerEC.text.length < 3,
+                child: DrawerContentWidget(
+                  editores: widget.editores,
+                  showEditores: widget.showEditores,
+                ),
               ),
-            ),
-            Visibility(
-              visible: controller.searchDrawerEC.text.length >= 3,
-              child: FutureBuilder<List<UserModel>>(
-                future: controller.searchByUser(),
-                builder: (context, snapshot) {
-                  final List<UserModel> editores = snapshot.data ?? [];
+              Visibility(
+                visible: controller.searchDrawerEC.text.length >= 3,
+                child: FutureBuilder<List<UserModel>>(
+                  future: controller.searchByUser(),
+                  builder: (context, snapshot) {
+                    final List<UserModel> editores = snapshot.data ?? [];
 
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CupertinoActivityIndicator());
-                  }
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CupertinoActivityIndicator());
+                    }
 
-                  return ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: editores.length,
-                    itemBuilder: (context, index) {
-                      return CardInfoWidget(
-                        showNet: false,
-                        showEdit: false,
-                        color: const Color(0XFF005E6C),
-                        editor: editores[index],
-                        isShearch: true,
-                      );
-                    },
-                  );
-                },
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: editores.length,
+                      itemBuilder: (context, index) {
+                        return CardInfoWidget(
+                          showNet: false,
+                          showEdit: false,
+                          color: const Color(0XFF005E6C),
+                          editor: editores[index],
+                          isShearch: true,
+                        );
+                      },
+                    );
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
